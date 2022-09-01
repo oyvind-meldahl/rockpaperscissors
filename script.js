60badcded970a8094a27b37c45ed79c41523aa9f
oyvind-meldahl <79719017+oyvind-meldahl@users.noreply.github.com>
2022-09-01 14:34:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const gameBoard = document.querySelector(".gameboard")
+const result = document.querySelector(".result")
+const tableComputer = document.querySelector(".computer-results")
+const tablePlayer = document.querySelector(".player-results")
+let playerScore = 0
+let computerScore = 0
+const hands = ["Rock", "Paper", "Scissors"]
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+const scissorsButton = document.querySelector(".scissors")
+
+var playersChoice = ""
+var aiChoice = ""
+
+rockButton.addEventListener("click", chooseRock)
+paperButton.addEventListener("click", choosePaper)
+scissorsButton.addEventListener("click", chooseScissors)
+
+function chooseRock() {
+    playersChoice = "Rock"
+     randomizer(hands)
+}
+
+function choosePaper() {
+    playersChoice = "Paper"
+    randomizer(hands)
+}
+
+function chooseScissors() {
+    playersChoice = "Scissors"
+    randomizer(hands)
+}
+
+function randomizer(hands) {
+     aiChoice = hands[Math.floor(Math.random()*hands.length)]
+     gameBoard.innerHTML = "The computer chose " + aiChoice
+
+     if (playersChoice === aiChoice) {
+        result.innerHTML = "You both chose the same thing, it's a TIE!"
+     } else if (playersChoice === "Rock" && aiChoice === "Scissors") {
+        result.innerHTML = "Rock beats scissors, YOU WIN!"
+        playerScore += 1
+        tablePlayer.innerHTML = playerScore
+     } else if (playersChoice === "Paper" && aiChoice === "Rock") {
+        result.innerHTML = "Paper beats rock, YOU WIN!"
+        playerScore += 1
+        tablePlayer.innerHTML = playerScore
+     } else if (playersChoice === "Scissors" && aiChoice === "Paper") {
+        result.innerHTML = "Scissors beat paper, YOU WIN!"
+        playerScore += 1
+        tablePlayer.innerHTML = playerScore
+     } else {
+        result.innerHTML = "You loooooose."
+        computerScore += 1
+        tableComputer.innerHTML = computerScore 
+     }
+}
